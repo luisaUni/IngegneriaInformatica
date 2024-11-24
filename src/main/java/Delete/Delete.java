ed0d60990776e9ed82176b8346a218ac3848f015
luuisaaaa <luisacrivo2003@gmail.com>
2024-11-24 19:59:41 +0100

    seconda prova commit

--- src/main/java/Delete/Delete.java
+++ src/main/java/Delete/Delete.java
@@ -15,7 +15,7 @@ public class Delete {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            System.out.println("ciao, LuisA");
+            System.out.println("ciao, Luisa");
         // TODO code application logic here
     }
     
