c4a3a17fdbc3e8ca63f2a6946afd92c37d9c8f16
luisaUni <l.crivo@studenti.unisa.it>
2024-12-06 16:35:33 +0100

    implementazione RubricaInterface

---
+++ ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/developed/RubricaInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.progettoinf.gruppo25.developed;
+
+/**
+ *
+ * @author luisacrivo
+ */
+public interface RubricaInterface {
+    public void aggiungiContatto(Contatto c);
+    public void eliminaContatto(Contatto c);
+    public Contatto cercaContatto(String s);
+    public void ordina(Rubrica e);
+}
