6a9af0649649cf1a0ecaf40f1f0be73f1036770b
luisaUni <l.crivo@studenti.unisa.it>
2024-12-08 18:51:39 +0100

    modifica classe Rubrica

--- ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/model/Rubrica.java
+++ ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/model/Rubrica.java
@@ -5,22 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
-<<<<<<< HEAD
- * @file Rubrica.java
- * @class Classe per rappresentare e gestire una rubrica di contatti.
+ *
+ *
+ * @class Rubrica.java
+ * @brief Classe per rappresentare e gestire una rubrica di contatti.
  *
  * Questa classe offre funzionalità per gestire una lista di contatti,
-=======
- * @class Rubrica.java
- * @brif Classe per rappresentare e gestire una rubrica di contatti.
- * 
- * Questa classe offre funzionalità per gestire una lista di contatti, 
->>>>>>> 2b98e25c09f0a552f6894c569517f0b02d0ef9a9
+ *
  * inclusa l'aggiunta, la rimozione, la ricerca e l'ordinamento.
  *
  * @author Luisa Crivo, Francesca De Pascale, Antonio Manuel Fedullo
  */
-public class Rubrica implemts RubricaInterface{
+public class Rubrica implements RubricaInterface{
     private List<Contatto> elenco;
 
     /**
