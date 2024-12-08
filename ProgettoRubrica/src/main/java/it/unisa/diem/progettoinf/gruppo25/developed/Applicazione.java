5534295d4ce6cb3629e855af85d5197c04a5bffe
luisaUni <l.crivo@studenti.unisa.it>
2024-12-08 10:49:40 +0100

    modifica metodidi rubrica e classe application come main class

--- ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/developed/Applicazione.java
+++ ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/developed/Applicazione.java
@@ -1,13 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package it.unisa.diem.progettoinf.gruppo25.developed;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
- * Hello world!
  *
+ * @author luisacrivo
  */
-public class Applicazione extends Application{
+public class Applicazione  extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
