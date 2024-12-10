e4161c228716866211452af300149a09bf6a5e89
Francesca <f.depascale8@studenti.unisa.it>
2024-12-10 11:25:22 +0100

    Implemento i metodi per la lettura e la scrittura.

--- ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/model/GestoreFile.java
+++ ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/model/GestoreFile.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package it.unisa.diem.progettoinf.gruppo25.model;
-import java.util.List;
+import java.util.ArrayList;
 import java.io.*;
 
 /**
@@ -15,6 +15,12 @@ import java.io.*;
  * @author Luisa Crivo, Francesca De Pascale, Antonio Manuel Fedullo
  */
 public class GestoreFile {
+  private static final String FILE_DEFAULT = "rubrica.csv";
+
+    public static String getFileDefault() {
+        return FILE_DEFAULT;
+    }
+
 
     /**
      * @brief Legge un file CSV e restituisce una lista di contatti.
@@ -27,9 +33,36 @@ public class GestoreFile {
      *
      * @param[in] filename Nome del file CSV da leggere.
      * @return Lista di contatti letta dal file CSV.
+     * @throws IOException Se si verifica un errore durante l'accesso al file.
      */
-    public List<Contatto> leggiCSV(String filename) throws IOException {
-        return null;
+    public static Rubrica importaCSV() throws IOException {
+      Rubrica r = new Rubrica();
+      try(BufferedReader br = new BufferedReader(new FileReader(file))){
+
+            if(br.readLine() == null){ // controlla se il file sia vuoto (riga nulla)
+                return r;
+            }
+
+            String line;
+            while ((line = br.readLine()) != null) {
+               String[] fields = line.split(";");
+               String nome = fields.length > 0 ? fields[0] : null;
+               String cognome = fields.length > 1 ? fields[1] : null;
+               String numero1 = fields.length > 2 ? fields[2] : null;
+               String numero2 = fields.length > 3 ? fields[3] : null;
+               String numero3 = fields.length > 4 ? fields[4] : null;
+               String email1 = fields.length > 5 ? fields[5] : null;
+               String email2 = fields.length > 6 ? fields[6] : null;
+               String email3 = fields.length > 7 ? fields[7] : null;
+
+               Contatto c = new Contatto(nome, cognome, numero1, numero2, numero3, email1, email2, email3);
+               r.aggiungiContatto(c);
+           }
+
+        }
+
+        return r;
+
     }
 
     /**
@@ -42,8 +75,105 @@ public class GestoreFile {
      *
      * @param[inout] filename Nome del file CSV in cui scrivere i dati.
      * @param[in] rubrica Lista di contatti da salvare nel file CSV.
+     * @throws IOException Se si verifica un errore durante l'accesso al file.
      */
-    public void scriviCSV(String filename, List<Contatto> rubrica) throws IOException {
+    public static void scriviCSV(Contatto c) throws IOException {
+      try(PrintWriter pw= new PrintWriter(new BufferedWriter(new FileWriter(file)))){
 
+        pw.println("NOME;COGNOME;NUMERO1;NUMERO2;NUMERO3;E-MAIL1;E-MAIL2;E-MAIL3");
+
+          if(c.getNome() != null){
+            pw.print(c.getNome());
+          }
+          pw.append(";");
+
+          if(c.getCognome()!= null){
+            pw.print(c.getCognome());
+          }
+          pw.append(";");
+
+          if(c.getNumero1()!= null){
+            pw.print(c.getNumero1());
+          }
+          pw.append(";");
+
+          if(c.getNumero2()!= null){
+              pw.print(c.getNumero2());
+          }
+          pw.append(";");
+
+          if(c.getNumero3()!= null){
+              pw.print(c.getNumero3());
+          }
+          pw.append(";");
+
+          if(c.getEmail1()!= null){
+              pw.print(c.getEmail1());
+          }
+          pw.append(";");
+
+          if(c.getEmail2()!= null){
+              pw.print(c.getEmail2());
+          }
+          pw.append(";");
+
+          if(c.getEmail3()!= null){
+              pw.print(c.getEmail3());
+          }
+            pw.append("\n");
+        }
+
+    }
+
+
+    public static void esporta(String filename, ArrayList<Contatto> rubrica) throws IOException{
+      try(PrintWriter pw= new PrintWriter(new BufferedWriter(new FileWriter(filename)))){
+
+        pw.println("NOME;COGNOME;NUMERO1;NUMERO2;NUMERO3;E-MAIL1;E-MAIL2;E-MAIL3");
+
+        for(Contatto c : rubrica){
+
+          if(c.getNome() != null){
+            pw.print(c.getNome());
+          }
+          pw.append(";");
+
+          if(c.getCognome()!= null){
+            pw.print(c.getCognome());
+          }
+          pw.append(";");
+
+          if(c.getNumero1()!= null){
+            pw.print(c.getNumero1());
+          }
+          pw.append(";");
+
+          if(c.getNumero2()!= null){
+              pw.print(c.getNumero2());
+          }
+          pw.append(";");
+
+          if(c.getNumero3()!= null){
+              pw.print(c.getNumero3());
+          }
+          pw.append(";");
+
+          if(c.getEmail1()!= null){
+              pw.print(c.getEmail1());
+          }
+          pw.append(";");
+
+          if(c.getEmail2()!= null){
+              pw.print(c.getEmail2());
+          }
+          pw.append(";");
+
+          if(c.getEmail3()!= null){
+              pw.print(c.getEmail3());
+          }
+            pw.append("\n");
+        }
+      }
+    }
     }
 }
