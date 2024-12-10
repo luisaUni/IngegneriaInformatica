174e74d36a76872bebfb1c4ac660ee9fc1ae7424
Antonio-Manuel03 <fedulloantoniomanuel.com>
2024-12-10 18:52:57 +0100

    creo view unica

--- ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/controller/ElencoViewController.java
+++ ProgettoRubrica/src/main/java/it/unisa/diem/progettoinf/gruppo25/controller/ElencoViewController.java
@@ -4,20 +4,22 @@
  * and open the template in the editor.
  */
 package it.unisa.diem.progettoinf.gruppo25.controller;
-import it.unisa.diem.progettoinf.gruppo25.app.Applicazione;
-import it.unisa.diem.progettoinf.gruppo25.model.Contatto;
-import java.io.IOException;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -26,10 +28,57 @@ import javafx.scene.layout.VBox;
  * @author antoniofedullo
  */
 public class ElencoViewController implements Initializable {
+
     @FXML
-    private AnchorPane contenitore;
+    private AnchorPane latoSinistro;
+    @FXML
+    private HBox hbox;
+    @FXML
+    private Button btnIndietro;
+    @FXML
+    private Label lblTitolo;
+    @FXML
+    private Button btnSalva;
+    @FXML
+    private VBox vbox;
+    @FXML
+    private Label lblNome;
+    @FXML
+    private TextField fieldNome;
+    @FXML
+    private Label lblCognome;
+    @FXML
+    private TextField fieldCognome;
+    @FXML
+    private Label lblNumero;
+    @FXML
+    private VBox containerNumeri;
+    @FXML
+    private HBox containerNumeroAdd;
+    @FXML
+    private TextField fieldNumero1;
+    @FXML
+    private Button btnAggiungiNumero;
+    @FXML
+    private Label lblEmail;
+    @FXML
+    private VBox containerEmail;
+    @FXML
+    private HBox containerEmailAdd;
+    @FXML
+    private TextField fieldEmail1;
+    @FXML
+    private Button btnAggiungiEmail;
+    @FXML
+    private Button btnPreferiti;
+    @FXML
+    private Button btnElimina;
+    @FXML
+    private AnchorPane latoDestro;
     @FXML
     private VBox VBox;
+    @FXML
+    private HBox HBox;
     @FXML
     private MenuBar menubar;
     @FXML
@@ -41,22 +90,21 @@ public class ElencoViewController implements Initializable {
     @FXML
     private MenuItem esporta;
     @FXML
-    private TextField tfdCerca;
+    private Button btnChiudi;
     @FXML
-    private TableView<Contatto> tblElenco;
+    private HBox containerCerca;
     @FXML
-    private TableColumn<Contatto, String> colonnaNome;
+    private ImageView imgCerca;
     @FXML
-    private TableColumn<Contatto, String> colonnaCognome;
+    private TextField fieldCerca;
     @FXML
-    private TableColumn<Contatto, Void> colonnaPreferito;
+    private TableView<?> tblElenco;
     @FXML
-    private TableColumn<Contatto, Void> colonnaInfo;
-
+    private TableColumn<?, ?> colonnaNome;
     @FXML
-    private void switchToSecondary() throws IOException{
-      //Applicazione.setRoot("secondary");
-    }
+    private TableColumn<?, ?> colonnaCognome;
+    @FXML
+    private TableColumn<?, ?> colonnaPreferito;
 
     /**
      * Initializes the controller class.
@@ -64,7 +112,6 @@ public class ElencoViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-
-    }
-
+    }    
+    
 }
